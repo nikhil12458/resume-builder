f76facd0e8836dd5d013f33d6479f119627711ba
Nikhil <ektak144@gmail.com>
2026-07-15 22:48:51 +0530

    feat: implement user authentication system with MongoDB, JWT, and bcrypt hashing

---
+++ src/models/User.model.ts
@@ -0,0 +1,50 @@
+import { IUser } from "@/types/user.types";
+import mongoose, { Document } from "mongoose";
+import bcrypt from "bcrypt";
+
+interface UserDocument extends Omit<IUser, "_id">, Document{
+  comparePass(candidatePassword: string): boolean
+}
+
+const userSchema = new mongoose.Schema<UserDocument>(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "password must be at least 6 characters"],
+    },
+    mobile: {
+      type: String,
+      minlength: [10, "min 10 digits required"],
+      maxlength: [10, "max 10 digits required"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", function (): void {
+  if (!this.isModified("password")) return;
+
+  this.password = bcrypt.hashSync(this.password, 10);
+});
+
+userSchema.methods.comparePass = function (candidatePassword: string): boolean {
+  return bcrypt.compareSync(candidatePassword, this.password);
+};
+
+const UserModel = mongoose.model("User", userSchema);
+
+export default UserModel;
